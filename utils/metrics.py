f15bd275f7848e17ecb48037b69efc63dcdaa7ab
Roman Korin <iamromankorin@gmail.com>
2026-06-22 10:31:19 +0300

    feat: create utils

---
+++ utils/metrics.py
@@ -0,0 +1,21 @@
+def jaccard_index(box_a: tuple[float, float, float, float], box_b: tuple[float, float, float, float]) -> float:
+    """Compute the Jaccard index (IoU) between two boxes in (x1, y1, x2, y2) format."""
+    ax1, ay1, ax2, ay2 = box_a
+    bx1, by1, bx2, by2 = box_b
+
+    inter_x1 = max(ax1, bx1)
+    inter_y1 = max(ay1, by1)
+    inter_x2 = min(ax2, bx2)
+    inter_y2 = min(ay2, by2)
+
+    inter_w = max(0.0, inter_x2 - inter_x1)
+    inter_h = max(0.0, inter_y2 - inter_y1)
+    inter_area = inter_w * inter_h
+
+    area_a = max(0.0, ax2 - ax1) * max(0.0, ay2 - ay1)
+    area_b = max(0.0, bx2 - bx1) * max(0.0, by2 - by1)
+    union_area = area_a + area_b - inter_area
+
+    if union_area <= 0:
+        return 0.0
+    return inter_area / union_area
